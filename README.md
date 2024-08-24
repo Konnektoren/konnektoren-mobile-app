fa721b34a70778490c0e9092e4a1d181b1748421
Christian M <chriamue@gmail.com>
2024-08-24 07:17:28 +0200

    :sparkles::lipstick: adds font awesome styles

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ This template should help get you started developing with Tauri and Yew.
 ## Recommended IDE Setup
 
 [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
+
+## Building
+
+```bash
+cargo tauri android build
+```
+
+## Running
+
+```bash
+cargo tauri android dev
+```
--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "help.konnektoren.app"
-        minSdk = 24
+        minSdk = tauriProperties.getProperty("tauri.android.minSdkVersion", "33").toInt()
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
